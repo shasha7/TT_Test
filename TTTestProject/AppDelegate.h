7561255c9231695b5025022fc69c7c8d6a7115d5
huhu <huchunyan7@gmail.com>
2018-03-31 17:46:54 +0800

    Initial Commit

---
+++ TTTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTTestProject
+//
+//  Created by hu hu on 2018/3/31.
+//  Copyright © 2018年 hu hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTTestProject
+//
+//  Created by hu hu on 2018/3/31.
+//  Copyright © 2018年 hu hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
